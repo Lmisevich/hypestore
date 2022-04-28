22b6f28be80f43c64a73d48b136ea0bee9ca85fb
lmisevich <z1869540@students.niu.edu>
2022-04-28 02:57:19 -0500

    Add cart.php

---
+++ html/cart.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types = 1);
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+?><!DOCTYPE html>
+<html>
+<head>
+     <title>Shopping cart</title>
+     <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
+</head>
+<body>
+     <h1> shopping cart</h1>
+
+     <div class="container-fluid">
+          <div class="col-md-12">
+               <div class="row">
+                    <div class="col-md-6">
+                         <h2 class="text-center">Shopping cart</h2>
+                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In enim nulla, malesuada nec convallis malesuada, fermentum non massa. Ut vulputate dolor a scelerisque facilisis. Fusce ut justo mi. Donec molestie sapien dui, non mollis ipsum varius et. Quisque ornare molestie sem, in mattis ligula accumsan vel. Praesent interdum, purus ut scelerisque aliquam, arcu urna hendrerit libero, sit amet volutpat elit odio sed justo. Ut ut metus vehicula, fermentum libero vel, tristique enim. Sed placerat consectetur mi, nec aliquam lacus posuere et. Aliquam vel commodo lacus, ac aliquam quam. Mauris a nibh at nisl elementum tempus. Vivamus iaculis vitae nibh sed tempus. Vestibulum cursus maximus sagittis. Duis vel tincidunt sem, vel condimentum orci. Donec sit amet enim condimentum, lacinia justo rhoncus, vehicula neque. Nunc egestas, velit quis consectetur suscipit, tellus ex tincidunt erat, non suscipit quam sapien semper lacus. </p>
+                    </div>
+                    <div class="col-md-6">
+                         <h2 class="text-center"> Item selected</h2>
+                         <p>Duis congue, nisi quis efficitur aliquet, sem mi lacinia augue, et varius ante ligula id justo. Donec et diam ante. Aenean tristique bibendum dui, ut iaculis odio aliquam quis. Sed bibendum luctus purus, vitae elementum ipsum semper suscipit. Praesent augue magna, commodo id lorem a, vehicula tristique risus. Proin scelerisque lobortis magna, id dignissim dui tempor non. Nulla ullamcorper, nibh id lobortis pretium, sapien diam posuere justo, nec hendrerit metus dolor sit amet eros. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Praesent rutrum pulvinar nulla ut sodales. Sed eleifend orci nulla, et egestas quam blandit a. </p>
+                    </div>
+               </div>
+          </div>
+     </div>
+
+</body>
+</html>
